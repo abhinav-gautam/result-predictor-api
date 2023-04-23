2d0bf00d8ad642c147ff2025546551a1bd4c9cbc
abhinav-gautam <abhinav22gautam@gmail.com>
2023-04-23 20:14:01 +0530

    refactor model loading

--- api.py
+++ api.py
@@ -7,24 +7,6 @@ import traceback
 
 app = Flask(__name__)
 CORS(app)
-# Load models
-dtc_model1 = joblib.load("./static/model1.pkl")  # Load "model.pkl"
-dtc_model2 = joblib.load("./static/model2.pkl")  # Load "model.pkl"
-dtc_model3 = joblib.load("./static/model3.pkl")  # Load "model.pkl"
-dtc_model4 = joblib.load("./static/model4.pkl")  # Load "model.pkl"
-dtc_model5 = joblib.load("./static/model5.pkl")  # Load "model.pkl"
-print('[+] Models loaded')
-# Load "model_columns.pkl"
-model1_columns = joblib.load("./static/model1_columns.pkl")
-# Load "model_columns.pkl"
-model2_columns = joblib.load("./static/model2_columns.pkl")
-# Load "model_columns.pkl"
-model3_columns = joblib.load("./static/model3_columns.pkl")
-# Load "model_columns.pkl"
-model4_columns = joblib.load("./static/model4_columns.pkl")
-# Load "model_columns.pkl"
-model5_columns = joblib.load("./static/model5_columns.pkl")
-print('[+] Model columns loaded')
 
 
 @app.route('/')
@@ -34,6 +16,10 @@ def index():
 
 @app.route('/predict_model1', methods=['POST'])
 def predict_model1():
+    # Load "model.pkl"
+    dtc_model1 = joblib.load("./static/model1.pkl")
+    # Load "model_columns.pkl"
+    model1_columns = joblib.load("./static/model1_columns.pkl")
     if dtc_model1:
         try:
             _json = request.json
@@ -41,7 +27,6 @@ def predict_model1():
             query = query.reindex(columns=model1_columns, fill_value=0)
 
             prediction = list(dtc_model1.predict(query))
-
             return jsonify({'prediction': str(prediction)})
 
         except:
@@ -54,6 +39,10 @@ def predict_model1():
 
 @app.route('/predict_model2', methods=['POST'])
 def predict_model2():
+    # Load "model.pkl"
+    dtc_model2 = joblib.load("./static/model2.pkl")
+    # Load "model_columns.pkl"
+    model2_columns = joblib.load("./static/model2_columns.pkl")
     if dtc_model2:
         try:
             _json = request.json
@@ -74,6 +63,10 @@ def predict_model2():
 
 @app.route('/predict_model3', methods=['POST'])
 def predict_model3():
+    # Load "model.pkl"
+    dtc_model3 = joblib.load("./static/model3.pkl")
+    # Load "model_columns.pkl"
+    model3_columns = joblib.load("./static/model3_columns.pkl")
     if dtc_model3:
         try:
             _json = request.json
@@ -94,6 +87,10 @@ def predict_model3():
 
 @app.route('/predict_model4', methods=['POST'])
 def predict_model4():
+    # Load "model.pkl"
+    dtc_model4 = joblib.load("./static/model4.pkl")
+    # Load "model_columns.pkl"
+    model4_columns = joblib.load("./static/model4_columns.pkl")
     if dtc_model4:
         try:
             _json = request.json
@@ -114,6 +111,10 @@ def predict_model4():
 
 @app.route('/predict_model5', methods=['POST'])
 def predict_model5():
+    # Load "model.pkl"
+    dtc_model5 = joblib.load("./static/model5.pkl")
+    # Load "model_columns.pkl"
+    model5_columns = joblib.load("./static/model5_columns.pkl")
     if dtc_model5:
         try:
             _json = request.json
